791122f16a63125ffdf84d8bdc310806944fdfff
TamizhJayapal <ajtamizh96@gmail.com>
2019-06-12 18:49:29 +0530

    authguard functionalities added

--- server/model/user.js
+++ server/model/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt  = require('bcryptjs');
 
-const user = new Schema({
+const userSchema = new Schema({
     name: {
         type:String,
         required:true
@@ -16,6 +17,20 @@ const user = new Schema({
     }
 });
 
-const userModel = mongoose.model('user', user);
+userSchema.methods.saveCredentials = function(credential){
+    var user = this;
+    var password = credential.password;
+    bcrypt.genSalt(10, function(err, salt){
+        bcrypt.hash(password, salt, function(e,hash){
+            user.password = hash;
+        });
+    });
+
+   return user.save(data).then((req,res)=>{
+        res.send(data);
+    })
+}
+
+const userModel = mongoose.model('userSchema', user);
 
 module.exports = userModel;
